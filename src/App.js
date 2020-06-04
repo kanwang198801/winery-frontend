89f04bf8abc2f7a72c1c88e308600605118f0b47
kan <kan.wang@arq.group>
2020-06-05 00:29:08 +1000

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Routes from './routes';
+import "./App.css";
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => (
--- src/containers/Wine.js
+++ src/containers/Wine.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
-import { message, Button, Input, Divider } from 'antd';
+import { Link } from 'react-router-dom';
+import { Descriptions, Divider, List } from 'antd';
 import 'antd/dist/antd.css';
-import { EyeOutlined, LoadingOutlined } from '@ant-design/icons';
+import { LoadingOutlined, ArrowLeftOutlined } from '@ant-design/icons';
 import Theme from "../components/Theme";
 import MockData from "../data/mockData.json";
 import { comparePercentage } from "../utils/functions"
@@ -22,13 +23,25 @@ function Wine(props) {
 
         let breakdown = [];
         let uniqueKey = [];
+
         searchResult.components.sort(comparePercentage).forEach(item => {
-            if (!uniqueKey.includes(`${item[type]} ${item[secondType]}`)) {
-                breakdown.push({
-                    percentage: item.percentage,
-                    key: `${item[type]} ${item[secondType]}`,
-                });
-                uniqueKey.push(`${item[type]} ${item[secondType]}`);
+            if (secondType) {
+                if (!uniqueKey.includes(`${item[type]} ${item[secondType]}`)) {
+                    breakdown.push({
+                        percentage: item.percentage,
+                        key: `${item[type]} ${item[secondType]}`,
+                    });
+                    uniqueKey.push(`${item[type]} ${item[secondType]}`);
+                }
+            }
+            else {
+                if (!uniqueKey.includes(`${item[type]}`)) {
+                    breakdown.push({
+                        percentage: item.percentage,
+                        key: `${item[type]}`,
+                    });
+                    uniqueKey.push(`${item[type]}`);
+                }
             }
         });
 
@@ -48,44 +61,93 @@ function Wine(props) {
                 <title>Wines</title>
                 <meta name="description" content="Wines" />
             </Helmet>
+            <Link to="/"><ArrowLeftOutlined /></Link>
             <h1>Wine Details</h1>
             {!loading ?
                 wine ?
                     <>
-                        <p><strong>Lot code - </strong>{wine.lotCode}</p>
-                        {wine.description && <p><strong>Description - </strong>{wine.description}</p>}
-                        <p><strong>Volume - </strong>{wine.volume} ML</p>
-                        <p><strong>Tank - </strong>{wine.tankCode}</p>
-                        {wine.productState && <p><strong> Product state - </strong>{wine.productState}</p>}
-                        <p><strong> Owner - </strong>{wine.ownerName}</p>
-                        <p><strong>Year - </strong></p>
-                        {yearBreakDownData.map(data =>
-                            <>
-                                <p>{data.key}</p>
-                                <p>Percentage: {data.percentage}</p>
-                            </>
-                        )}
-                        <p><strong>Variety - </strong></p>
-                        {varietyBreakdownData.map(data =>
-                            <>
-                                <p>{data.key}</p>
-                                <p>Percentage: {data.percentage}</p>
-                            </>
-                        )}
-                        <p><strong>Region - </strong></p>
-                        {regionBreakdownData.map(data =>
-                            <>
-                                <p>{data.key}</p>
-                                <p>Percentage: {data.percentage}</p>
-                            </>
-                        )}
-                        <p><strong>Year + Variety - </strong></p>
-                        {yearAndVarietyBreakdownData.map(data =>
-                            <>
-                                <p>{data.key}</p>
-                                <p>Percentage: {data.percentage}</p>
-                            </>
-                        )}
+                        <Divider />
+                        <Descriptions title="Wine Information" bordered>
+                            <Descriptions.Item label="Lot code"> {wine.lotCode}</Descriptions.Item>
+                            {wine.description && <Descriptions.Item label="Product state">{wine.description}</Descriptions.Item>}
+                            <Descriptions.Item label="Volume">{wine.volume}</Descriptions.Item>
+                            <Descriptions.Item label="Tank">{wine.tankCode}</Descriptions.Item>
+                            {wine.productState && <Descriptions.Item label="Product state">{wine.productState}</Descriptions.Item>}
+                            <Descriptions.Item label="Owner">{wine.ownerName}</Descriptions.Item>
+                        </Descriptions>
+                        <Divider />
+                        <h3><strong>Year</strong></h3>
+                        <List
+                            itemLayout="vertical"
+                            size="large"
+                            pagination={{
+                                onChange: page => {
+                                    console.log(page);
+                                },
+                                pageSize: 5,
+                                hideOnSinglePage: true
+                            }}
+                            dataSource={yearBreakDownData}
+                            renderItem={item => (
+                                <List.Item key={item.key}>
+                                    {item.key} -  {item.percentage}%
+                                </List.Item>
+                            )}
+                        />
+                        <h3><strong>Variety</strong></h3>
+                        <List
+                            itemLayout="vertical"
+                            size="large"
+                            pagination={{
+                                onChange: page => {
+                                    console.log(page);
+                                },
+                                pageSize: 5,
+                                hideOnSinglePage: true
+                            }}
+                            dataSource={varietyBreakdownData}
+                            renderItem={item => (
+                                <List.Item key={item.key}>
+                                    {item.key} -  {item.percentage}%
+                                </List.Item>
+                            )}
+                        />
+                        <h3><strong>Region</strong></h3>
+                        <List
+                            itemLayout="vertical"
+                            size="large"
+                            pagination={{
+                                onChange: page => {
+                                    console.log(page);
+                                },
+                                pageSize: 5,
+                                hideOnSinglePage: true
+                            }}
+                            dataSource={regionBreakdownData}
+                            renderItem={item => (
+                                <List.Item key={item.key}>
+                                    {item.key} -  {item.percentage}%
+                                </List.Item>
+                            )}
+                        />
+                        <h3><strong>Year + Variety</strong></h3>
+                        <List
+                            itemLayout="vertical"
+                            size="large"
+                            pagination={{
+                                onChange: page => {
+                                    console.log(page);
+                                },
+                                pageSize: 5,
+                                hideOnSinglePage: true
+                            }}
+                            dataSource={yearAndVarietyBreakdownData}
+                            renderItem={item => (
+                                <List.Item key={item.key}>
+                                    {item.key} -  {item.percentage}%
+                                </List.Item>
+                            )}
+                        />
                     </>
                     :
                     <LoadingOutlined style={{ fontSize: '36px' }} />
